884cbc1dd05553cbae7b782c3fcdb6b86c806340
Piotr Chrusciel <chrusciel.piotr.87@gmail.com>
2019-05-17 00:47:29 +0200

    Remove next.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const path = require('path')
-const dotenv = require('dotenv');
-
-const config = dotenv.config();
-
-module.exports = require( path.resolve(__dirname, process.env.CONFIG_DIR, 'next.config') );
---
+++ server/controller/indexController.tsx
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+const indexController = (req: Request, res: Response) => {
+    res.send({ test: 1 }.toString());
+};
+
+export default indexController;
