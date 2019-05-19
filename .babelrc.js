0a19600ee1082b4828d147b9b43c931d12733796
Piotr Chrusciel <chrusciel.piotr.87@gmail.com>
2019-05-19 19:03:44 +0200

    Add babelrc configuration: borrowed from manuelbieh/react-ssr-setup

--- .babelrc.js
+++ .babelrc.js
@@ -1,10 +1,38 @@
 const path = require('path');
 
 const presets = [
+    [
+        '@babel/env',
+        {
+            modules: false,
+            targets: {
+                browsers: ['last 2 versions', 'ie >= 9'],
+            }
+        }
+    ],
     "next/babel",
     "@zeit/next-typescript/babel"
 ]
 
+const plugins = [
+    '@babel/proposal-object-rest-spread',
+    '@babel/proposal-class-properties',
+    '@babel/proposal-optional-chaining',
+    '@babel/syntax-dynamic-import',
+    'macros',
+];
+
+const env = {
+    test: {
+        plugins: [
+            '@babel/transform-modules-commonjs',
+            '@babel/syntax-dynamic-import'
+        ]
+    }
+};
+
 module.exports = {
-    presets: presets
+    presets: presets,
+    plugins: plugins,
+    env: env
 }
